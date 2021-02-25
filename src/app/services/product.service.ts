f35a4ea534d146807124f6e4b0e4f0443945b57a
Benjamin Valin <chbench1502@gmail.com>
2021-02-25 11:02:10 +0100

    ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private BASE_URL = 'http://localhost:8080/products'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.BASE_URL);
+  }
+
+  getByID(id): Observable<Product> {
+    return this.httpClient.get<Product>(this.BASE_URL + "/" + id);
+  }
+
+  delete(id): Observable<Product> {
+    return this.httpClient.delete<Product>(this.BASE_URL + "/" + id);
+  }
+
+  insert(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(this.BASE_URL, product);
+  }
+
+  update(id, product: Product): Observable<Product> {
+    return this.httpClient.put<Product>(this.BASE_URL + "/" + id, product);
+  }
 }
