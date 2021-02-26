955f9344fe52e81d7f913975d7b9848553bada6f
Benjamin Valin <chbench1502@gmail.com>
2021-02-26 09:34:52 +0100

    Travail sur formulaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product/product-detail/product-detail.component';
+import { ProductAddComponent } from './components/product/product-add/product-add.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'products', component: ProductComponent, children: [{
     path: 'detail/:id',
     component: ProductDetailComponent
-  }]},
+  },{
+    path: 'add',
+    component: ProductAddComponent
+  }
+]},
   {path: '**', redirectTo: 'home'}
 ];
 
