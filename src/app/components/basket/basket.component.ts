24c30cd7740be634cc666437dd8b39c92d09584b
Benjamin Valin <chbench1502@gmail.com>
2021-03-02 16:47:09 +0100

    Gestion du panier

--- src/app/components/basket/basket.component.ts
+++ src/app/components/basket/basket.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 
 @Component({
@@ -11,10 +12,33 @@ import { Product } from 'src/app/models/product.model';
 export class BasketComponent implements OnInit {
 
   products$ : Observable<Product[]>;
+  cart : any[];
+  cartSubscription : Subscription;
 
-  constructor() { }
+  constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
+    // this.cartService.cartSub.subscribe(d => this.cart = d);
+    this.cart = this.cartService.showCart();
+  }
+
+  refresh(){
+    // this.cartService.cartSub.subscribe(d => this.cart = d);
+    this.cart = this.cartService.showCart();
+  }
+
+  remove(product: Product){
+    this.cartService.remove(product)
+  }
+
+  oneMore(product: Product){
+    this.cartService.oneMore(product);
+    this.refresh();
+  }
+
+  oneLess(product: Product){
+    this.cartService.oneLess(product);
+    this.refresh();
   }
 
 }
