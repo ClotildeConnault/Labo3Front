5f36ac37807f0c86b4a879cafbd384062fe41d6c
Benjamin Valin <chbench1502@gmail.com>
2021-04-12 12:39:10 +0200

    Log des produits v1.5
    Ajout de variations selon demande de l'historique d'un produit, ou de l'historique d'un utilisateur

--- src/app/components/product-log/product-log.component.ts
+++ src/app/components/product-log/product-log.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
 import { ProductLog } from 'src/app/models/productLog.model';
+import { User } from 'src/app/models/user.model';
 import { ProductLogService } from 'src/app/services/product-log.service';
+import { ProductService } from 'src/app/services/product.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-product-log',
@@ -11,20 +15,27 @@ import { ProductLogService } from 'src/app/services/product-log.service';
 export class ProductLogComponent implements OnInit {
 
   plog : ProductLog[]
+  entry : string;
+  prod : Product;
+  user : User;
 
-  constructor(private activatedRoute : ActivatedRoute, private logService : ProductLogService) { }
+  constructor(private activatedRoute : ActivatedRoute, private logService : ProductLogService, private prodService : ProductService, private userService : UserService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(data => {
       this.activatedRoute.url.subscribe(url => {
         if (url[1].toString() == "product") {
+          this.entry = "product"
           this.logService.getLogsBySpecificProduct(data.id).subscribe(p => {
             this.plog = p;
-          })
+          });
+          this.prodService.getByID(data.id).subscribe(p => this.prod = p);
         } else {
+          this.entry = "user"
           this.logService.getLogsBySpecificUser(data.id).subscribe(p => {
             this.plog = p;
-          })
+          });
+          this.userService.getByID(data.id).subscribe(u => this.user = u);
         }
       })
     })
