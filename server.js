f1c47062ea80c620fa97b93de35b717ed8a43798
Benjamin Valin <chbench1502@gmail.com>
2021-04-19 17:09:12 +0200

    Correction nom projet server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/labo3-front'));
+app.use(express.static(__dirname + '/dist/Labo3Front'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/labo3-front/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Labo3Front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
